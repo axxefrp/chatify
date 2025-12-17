ebf5c1e1d38441952138b0ec7856be9bc741fe20
axxefrp <axxefrp@gmail.com>
2025-12-17 11:32:51 +0000

    Fix cross-domain cookie issue for production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    sameSite: "none", // Allow cross-domain in production
+    secure: true, // Always use HTTPS in production
   });
 
   return token;
